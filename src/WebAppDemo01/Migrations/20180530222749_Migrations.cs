179562f7c0b5293649b8048101c2cc19c45cb617
Alumno 36 <Alumno 36@LAB04-PC36>
2018-05-30 17:03:06 -0600

    Agregar archivos de proyecto.

---
+++ src/WebAppDemo01/Migrations/20180530222749_Migrations.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebAppDemo01.Migrations
+{
+    public partial class Migrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CategoriasProductos",
+                columns: table => new
+                {
+                    CodigoCatProducto = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DescripcionCatProducto = table.Column<string>(nullable: true),
+                    NombreCatProducto = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoriasProductos", x => x.CodigoCatProducto);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CategoriasUsuarios",
+                columns: table => new
+                {
+                    CodigoCatUo = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DescripcionCatUo = table.Column<string>(nullable: true),
+                    NombreCatUo = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoriasUsuarios", x => x.CodigoCatUo);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClientesDetalle",
+                columns: table => new
+                {
+                    CodigoCliDetalle = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DetalleCliente = table.Column<string>(nullable: true),
+                    NombreCliDetalle = table.Column<string>(nullable: true),
+                    TipoCliente = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientesDetalle", x => x.CodigoCliDetalle);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Proveedores",
+                columns: table => new
+                {
+                    CodigoProveedores = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CargoContacto = table.Column<string>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    ImagenURL = table.Column<string>(nullable: true),
+                    NombreContacto = table.Column<string>(nullable: true),
+                    NombreProveedor = table.Column<string>(nullable: true),
+                    Notas = table.Column<string>(nullable: true),
+                    ProveedoresInactivos = table.Column<bool>(nullable: false),
+                    Telefono = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proveedores", x => x.CodigoProveedores);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    CodigoProducto = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CatProductosCodigoCatProducto = table.Column<int>(nullable: true),
+                    DescripProducto = table.Column<string>(nullable: true),
+                    EstadoProducto = table.Column<bool>(nullable: false),
+                    ImagenURL = table.Column<string>(nullable: true),
+                    NombreProducto = table.Column<string>(nullable: true),
+                    PreCostoProducto = table.Column<decimal>(nullable: false),
+                    PreVentaProducto = table.Column<decimal>(nullable: false),
+                    ProductoEnExistencia = table.Column<bool>(nullable: false),
+                    ProductoEnOferta = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.CodigoProducto);
+                    table.ForeignKey(
+                        name: "FK_Productos_CategoriasProductos_CatProductosCodigoCatProducto",
+                        column: x => x.CatProductosCodigoCatProducto,
+                        principalTable: "CategoriasProductos",
+                        principalColumn: "CodigoCatProducto",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    CodigoUsuario = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ApellidoUsuario = table.Column<string>(nullable: true),
+                    CatUsuariosCodigoCatUo = table.Column<int>(nullable: true),
+                    Contraseña = table.Column<string>(nullable: true),
+                    ImagenUsuarioURL = table.Column<string>(nullable: true),
+                    ListaNegra = table.Column<bool>(nullable: false),
+                    NombreUsuario = table.Column<string>(nullable: true),
+                    UserName = table.Column<string>(nullable: true),
+                    UsuariosInactivos = table.Column<bool>(nullable: false),
+                    fechacreacion = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.CodigoUsuario);
+                    table.ForeignKey(
+                        name: "FK_Usuarios_CategoriasUsuarios_CatUsuariosCodigoCatUo",
+                        column: x => x.CatUsuariosCodigoCatUo,
+                        principalTable: "CategoriasUsuarios",
+                        principalColumn: "CodigoCatUo",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    CodigoClientes = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ApellidoClientes = table.Column<string>(nullable: true),
+                    ClientesDetalleCodigoCliDetalle = table.Column<int>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    FechaNacimiento = table.Column<string>(nullable: true),
+                    Imagenes = table.Column<string>(nullable: true),
+                    NombreClientes = table.Column<string>(nullable: true),
+                    NumDocumento = table.Column<int>(nullable: false),
+                    Sexo = table.Column<string>(nullable: true),
+                    Telefono = table.Column<int>(nullable: false),
+                    TipoDocumento = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.CodigoClientes);
+                    table.ForeignKey(
+                        name: "FK_Clientes_ClientesDetalle_ClientesDetalleCodigoCliDetalle",
+                        column: x => x.ClientesDetalleCodigoCliDetalle,
+                        principalTable: "ClientesDetalle",
+                        principalColumn: "CodigoCliDetalle",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_ClientesDetalleCodigoCliDetalle",
+                table: "Clientes",
+                column: "ClientesDetalleCodigoCliDetalle");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_CatProductosCodigoCatProducto",
+                table: "Productos",
+                column: "CatProductosCodigoCatProducto");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_CatUsuariosCodigoCatUo",
+                table: "Usuarios",
+                column: "CatUsuariosCodigoCatUo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "Proveedores");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "ClientesDetalle");
+
+            migrationBuilder.DropTable(
+                name: "CategoriasProductos");
+
+            migrationBuilder.DropTable(
+                name: "CategoriasUsuarios");
+        }
+    }
+}
---
+++ src/WebAppDemo01/Migrations/AppDbContextModelSnapshot.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using WebAppDemo01.models;
+
+namespace WebAppDemo01.Migrations
+{
+    [DbContext(typeof(AppDbContext))]
+    partial class AppDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("WebAppDemo01.models.CatProductos", b =>
+                {
+                    b.Property<int>("CodigoCatProducto")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("DescripcionCatProducto");
+
+                    b.Property<string>("NombreCatProducto");
+
+                    b.HasKey("CodigoCatProducto");
+
+                    b.ToTable("CategoriasProductos");
+                });
+
+            modelBuilder.Entity("WebAppDemo01.models.CatUsuarios", b =>
+                {
+                    b.Property<int>("CodigoCatUo")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("DescripcionCatUo");
+
+                    b.Property<string>("NombreCatUo");
+
+                    b.HasKey("CodigoCatUo");
+
+                    b.ToTable("CategoriasUsuarios");
+                });
+
+            modelBuilder.Entity("WebAppDemo01.models.Clientes", b =>
+                {
+                    b.Property<int>("CodigoClientes")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ApellidoClientes");
+
+                    b.Property<int?>("ClientesDetalleCodigoCliDetalle");
+
+                    b.Property<string>("Direccion");
+
+                    b.Property<string>("Email");
+
+                    b.Property<string>("FechaNacimiento");
+
+                    b.Property<string>("Imagenes");
+
+                    b.Property<string>("NombreClientes");
+
+                    b.Property<int>("NumDocumento");
+
+                    b.Property<string>("Sexo");
+
+                    b.Property<int>("Telefono");
+
+                    b.Property<string>("TipoDocumento");
+
+                    b.HasKey("CodigoClientes");
+
+                    b.HasIndex("ClientesDetalleCodigoCliDetalle");
+
+                    b.ToTable("Clientes");
+                });
+
+            modelBuilder.Entity("WebAppDemo01.models.ClientesDetalle", b =>
+                {
+                    b.Property<int>("CodigoCliDetalle")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("DetalleCliente");
+
+                    b.Property<string>("NombreCliDetalle");
+
+                    b.Property<string>("TipoCliente");
+
+                    b.HasKey("CodigoCliDetalle");
+
+                    b.ToTable("ClientesDetalle");
+                });
+
+            modelBuilder.Entity("WebAppDemo01.models.Productos", b =>
+                {
+                    b.Property<int>("CodigoProducto")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("CatProductosCodigoCatProducto");
+
+                    b.Property<string>("DescripProducto");
+
+                    b.Property<bool>("EstadoProducto");
+
+                    b.Property<string>("ImagenURL");
+
+                    b.Property<string>("NombreProducto");
+
+                    b.Property<decimal>("PreCostoProducto");
+
+                    b.Property<decimal>("PreVentaProducto");
+
+                    b.Property<bool>("ProductoEnExistencia");
+
+                    b.Property<bool>("ProductoEnOferta");
+
+                    b.HasKey("CodigoProducto");
+
+                    b.HasIndex("CatProductosCodigoCatProducto");
+
+                    b.ToTable("Productos");
+                });
+
+            modelBuilder.Entity("WebAppDemo01.models.Proveedores", b =>
+                {
+                    b.Property<int>("CodigoProveedores")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("CargoContacto");
+
+                    b.Property<string>("Direccion");
+
+                    b.Property<string>("Email");
+
+                    b.Property<string>("ImagenURL");
+
+                    b.Property<string>("NombreContacto");
+
+                    b.Property<string>("NombreProveedor");
+
+                    b.Property<string>("Notas");
+
+                    b.Property<bool>("ProveedoresInactivos");
+
+                    b.Property<int>("Telefono");
+
+                    b.HasKey("CodigoProveedores");
+
+                    b.ToTable("Proveedores");
+                });
+
+            modelBuilder.Entity("WebAppDemo01.models.Usuarios", b =>
+                {
+                    b.Property<int>("CodigoUsuario")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ApellidoUsuario");
+
+                    b.Property<int?>("CatUsuariosCodigoCatUo");
+
+                    b.Property<string>("Contraseña");
+
+                    b.Property<string>("ImagenUsuarioURL");
+
+                    b.Property<bool>("ListaNegra");
+
+                    b.Property<string>("NombreUsuario");
+
+                    b.Property<string>("UserName");
+
+                    b.Property<bool>("UsuariosInactivos");
+
+                    b.Property<string>("fechacreacion");
+
+                    b.HasKey("CodigoUsuario");
+
+                    b.HasIndex("CatUsuariosCodigoCatUo");
+
+                    b.ToTable("Usuarios");
+                });
+
+            modelBuilder.Entity("WebAppDemo01.models.Clientes", b =>
+                {
+                    b.HasOne("WebAppDemo01.models.ClientesDetalle", "ClientesDetalle")
+                        .WithMany("Clientes")
+                        .HasForeignKey("ClientesDetalleCodigoCliDetalle");
+                });
+
+            modelBuilder.Entity("WebAppDemo01.models.Productos", b =>
+                {
+                    b.HasOne("WebAppDemo01.models.CatProductos", "CatProductos")
+                        .WithMany("Productos")
+                        .HasForeignKey("CatProductosCodigoCatProducto");
+                });
+
+            modelBuilder.Entity("WebAppDemo01.models.Usuarios", b =>
+                {
+                    b.HasOne("WebAppDemo01.models.CatUsuarios", "CatUsuarios")
+                        .WithMany("Usuarios")
+                        .HasForeignKey("CatUsuariosCodigoCatUo");
+                });
+        }
+    }
+}
